3fc6c4241ac029fecd2c035511a81f4afc3cec93
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-15 09:20:25 -0700

    Create tests for has student info.

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -8,4 +8,25 @@ RSpec.describe Student do
       expect(student).to be_a(Student)
     end
   end
+
+  describe 'has student info' do
+    before(:each) do
+      @student = Student.new({name: "Morgan", age: 21})
+    end
+    it '#name' do
+      expect(@student.name).to eq("Morgan")
+    end
+    
+    it '#age' do
+      expect(@student.age).to eq(21)
+    end
+
+    it '#scores' do
+      expect(@student.scores).to eq([])
+    end
+
+    xit '#grade' do
+      expect(@student.grade).to eq(83.5)
+    end
+  end
 end
