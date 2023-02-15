0ad5ef95874ce1f231758380c74cfd2da83934bf
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-15 10:13:41 -0700

    Create methods for full? and enroll.

--- lib/course.rb
+++ lib/course.rb
@@ -1,1 +1,27 @@
+require './lib/student'
 
+class Course
+  attr_reader :name, :capacity, :students
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @students = []
+    @full = false
+  end
+
+  def full?
+    if @students.size < @capacity
+      @full = false
+    else
+      @full = true
+    end
+  end
+
+  def enroll(student)
+    if @students.size < @capacity
+      @students << student
+    else
+      "no room"
+    end
+  end
+end
