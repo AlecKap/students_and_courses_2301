69a627c9084b77a0eebb523c4840c648afefc7dd
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-15 09:20:55 -0700

    Add instance variables.

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,10 @@
 
 class Student
+  attr_reader :name, :age, :scores, :grade
   def initialize(student_info)
     @name = student_info[:name]
     @age = student_info[:age]
+    @scores = []
+    @grade = 0
   end
 end
