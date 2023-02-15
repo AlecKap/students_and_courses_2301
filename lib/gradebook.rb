54d53224ee9b783ca4414310d1e19bf4cdeea476
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-15 10:35:05 -0700

    Create gradebook class and give it instance variables.

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,1 +1,23 @@
+require './lib/course'
+require './lib/student'
 
+class Gradebook
+  attr_reader :courses, :instructor
+  def initialize(instructor)
+    @courses = []
+    @instructor = instructor
+
+  end
+
+  def add_courses(course)
+    #code
+  end
+
+  def list_all_courses
+    #code
+  end
+
+  def students_below
+    #code
+  end
+end
